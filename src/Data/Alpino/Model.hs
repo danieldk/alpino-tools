f9e37d5bc36ed8e63a2cf0c2a4a72e3937b3f764
Daniel de Kok <me@danieldk.eu>
2010-10-19 20:31:11 +0200

    Add more documentation.

--- src/Data/Alpino/Model.hs
+++ src/Data/Alpino/Model.hs
@@ -12,18 +12,22 @@ import Data.ByteString.Lex.Double (readDouble)
 import qualified Data.ByteString.UTF8 as BU
 import Data.Maybe (fromJust)
 
+
+-- | A training instance.
+data TrainingInstance = TrainingInstance {
+      instanceType :: TrainingInstanceType, -- ^ Type of training instance
+      key          :: B.ByteString,         -- ^ Training instance identifier
+      n            :: B.ByteString,
+      score        :: Double,               -- ^ Quality score
+      features     :: B.ByteString          -- ^ Features
+} deriving (Show, Eq)
+
+-- | Type of training instance (parsing or generation)
 data TrainingInstanceType =
     ParsingInstance | GenerationInstance
     deriving (Show, Eq)
 
-data TrainingInstance = TrainingInstance {
-      instanceType :: TrainingInstanceType,
-      key          :: B.ByteString,
-      n            :: B.ByteString,
-      score        :: Double,
-      features     :: B.ByteString
-} deriving (Show, Eq)
-
+-- | Read a training instance from a ByteString.
 bsToTrainingInstance :: B.ByteString -> TrainingInstance
 bsToTrainingInstance l =
     TrainingInstance instType key n score features
@@ -34,6 +38,7 @@ bsToTrainingInstance l =
           score = fst . fromJust . readDouble $ lineParts !! 3
           features = lineParts !! 4
 
+-- | Convert a training instance to a ByteString.
 trainingInstanceToBs :: TrainingInstance -> B.ByteString
 trainingInstanceToBs i = B.append typeBS $ B.append fieldSep $
                          B.append keyBS $ B.append fieldSep $
@@ -61,6 +66,9 @@ typeToBS instanceType
     | instanceType == ParsingInstance = parseMarker
     | instanceType == GenerationInstance = generationMarker
 
+-- | Convert the quality scores to binary scores. The instances
+-- | with the highest quality score get score 1.0, other instances
+-- | get score 0.0.
 scoreToBinary :: [TrainingInstance] -> [TrainingInstance]
 scoreToBinary ctx = map (rescoreEvt maxScore) ctx
     where maxScore = foldl (\acc e -> max acc $ score e) 0.0 ctx
@@ -68,6 +76,8 @@ scoreToBinary ctx = map (rescoreEvt maxScore) ctx
             | score evt == maxScore = evt { score = 1.0 }
             | otherwise = evt { score = 0.0 }
 
+-- | Divide a score of 1.0 uniformly over instances with the highest
+-- | quality scores.
 scoreToBinaryNorm :: [TrainingInstance] -> [TrainingInstance]
 scoreToBinaryNorm ctx = map (rescoreEvt maxScore) ctx
     where maxScore = foldl (\acc e -> max acc $ score e) 0.0 ctx
--- src/Data/Alpino/Model/Enumerator.hs
+++ src/Data/Alpino/Model/Enumerator.hs
@@ -18,15 +18,17 @@ import qualified Data.Enumerator as E
 import Data.Enumerator hiding (isEOF, map)
 import System.IO (isEOF)
 
-
+-- | Enumeratee that converts ByteStrings to TrainingInstances.
 instanceParser :: (Monad m) =>
                   Enumeratee BU.ByteString AM.TrainingInstance m b
 instanceParser = E.map AM.bsToTrainingInstance
 
+-- | Enumeratee that converts TrainingInstances to ByteStrings.
 instanceGenerator :: (Monad m) =>
                      Enumeratee AM.TrainingInstance B.ByteString m b
 instanceGenerator = E.map AM.trainingInstanceToBs
 
+-- | Enumerator of lines read from the standard input.
 lineEnum :: MonadIO m => Enumerator B.ByteString m b
 lineEnum = Iteratee . loop
     where loop (Continue k) = do
@@ -38,6 +40,7 @@ lineEnum = Iteratee . loop
                        runIteratee (k (Chunks [line])) >>= loop
           loop step = return step
 
+-- | Group training instances by type and key.
 groupBy :: (Monad m) =>
                   Enumeratee AM.TrainingInstance [AM.TrainingInstance] m b
 groupBy = loop
@@ -53,6 +56,7 @@ groupBy = loop
           keyEq i1 i2 = AM.instanceType i1 == AM.instanceType i2 &&
                                  AM.key i1 == AM.key i2
 
+-- | Enumeratee concatenating lists.
 concat :: (Monad m) =>
               Enumeratee [a] a m b
 concat = loop
@@ -65,16 +69,21 @@ concat = loop
                      loop newStep
           loop step = return step
 
+-- | Iterator printing ByteStrings to the standard output.
 printByteString :: MonadIO m => Iteratee B.ByteString m ()
 printByteString = continue step
     where step (Chunks []) = continue step
           step (Chunks xs) = liftIO (mapM_ B.putStrLn xs) >> continue step
           step EOF = yield () EOF
 
+-- | Enumerator recaculating scores to binary scores (1.0 for best,
+-- | 0.0 for the rest).
 scoreToBinary :: (Monad m) =>
                  Enumeratee [AM.TrainingInstance] [AM.TrainingInstance] m b
 scoreToBinary = E.map AM.scoreToBinary
 
+-- | Enumerator recalculating scores, dividing a score of 1.0 uniformly
+-- | over instances with the highest quality score.
 scoreToBinaryNorm :: (Monad m) =>
                      Enumeratee [AM.TrainingInstance] [AM.TrainingInstance] m b
 scoreToBinaryNorm = E.map AM.scoreToBinaryNorm
