a0d5ebfeb4b785681e80705f45a717adb9b69614
Daniël de Kok <me@danieldk.eu>
2012-01-10 15:36:37 +0100

    Add support for Bytestring splitting.

--- src/Data/Alpino/Model/Conduit.hs
+++ src/Data/Alpino/Model/Conduit.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DoAndIfThenElse #-}
 
 module Data.Alpino.Model.Conduit (
   bestScore,
@@ -7,15 +8,25 @@ module Data.Alpino.Model.Conduit (
   groupBy,
   groupByKey,
   bsToTrainingInstance,
+  drop,
+  splitWith,
+  take,
+  takeWhile,
   trainingInstanceToBS
 ) where
 
+import           Prelude hiding (drop, take, takeWhile)
 import           Control.Exception.Base (Exception)
+import           Control.Monad (liftM)
 import qualified Data.Alpino.Model as AM
 import qualified Data.ByteString as B
-import Data.Conduit (Conduit, ConduitResult(..), Resource(..), conduitState)
+import           Data.Conduit (Conduit, ConduitResult(..), Resource(..),
+                   SequencedSinkResponse(Emit), Sink(..), SinkResult(..),
+                   conduitState, sequenceSink, sinkState)
 import qualified Data.Conduit.List as CL
+import           Data.Maybe (isNothing)
 import qualified Data.Set as Set
+import           Data.Word (Word8)
 import Data.Typeable
 import Control.Monad.Trans.Resource (ResourceThrow (..))
 
@@ -67,6 +78,67 @@ groupByKey :: Resource m =>
 groupByKey = groupBy keyEq
     where keyEq i1 i2 = AM.instanceType i1 == AM.instanceType i2 &&
                         AM.instanceKey i1 == AM.instanceKey i2
+
+drop :: Resource m => Int -> Sink B.ByteString m ()
+drop n =
+  sinkState
+    n
+    push
+    close
+  where
+    push 0 x     = return (0, Done (Just x) ())
+    push count v
+      | bsLen <= count = return (count - bsLen, Processing)
+      | otherwise      = return (0, Done (Just $ B.drop count v) ())
+      where
+        bsLen  = B.length v
+    close _ = return ()
+
+take :: Resource m => Int -> Sink B.ByteString m B.ByteString
+take n =
+  sinkState
+    (n, B.empty)
+    push
+    close
+  where
+    push (0,     acc) v = return ((0, B.empty), Done (Just v) acc)
+    push (count, acc) v
+      | bsLen <= count  =
+        return ((count - bsLen, B.append acc v), Processing)
+      | otherwise       =
+        let (xs, rest) = B.splitAt count v in
+          return ((0, acc), Done (Just rest) (B.append acc xs))
+      where
+        bsLen  = B.length v
+    close (_, acc) = return acc
+
+
+takeWhile :: Resource m => (Word8 -> Bool) -> Sink B.ByteString m B.ByteString
+takeWhile f =
+  sinkState
+    B.empty
+    push
+    close
+  where
+    push s v =
+      if B.null rest then
+        return (B.append s v, Processing)
+      else
+        return (B.empty, Done (Just rest) (B.append s match))
+      where
+        (match, rest) = B.span f v
+    close s = return s
+
+splitWith :: Resource m => (Word8 -> Bool) -> Conduit B.ByteString m B.ByteString
+splitWith f =
+  sequenceSink () $ \() -> do
+    nextElem <- takeWhile f
+    sep <- take 1 -- Drop splitting character (if any)
+    eof <- isNothing `liftM` CL.peek
+    if B.length sep == 1 && eof then
+      return $ Emit () [nextElem, B.empty]
+    else
+      return $ Emit () [nextElem]
 
 -- XXX: Use proper serialization?
 bsToTrainingInstance :: ResourceThrow m => Conduit B.ByteString m AM.TrainingInstance
